2ed1ef6104c35e1feac6b371b902674445ca040f
uedaryota <mr.335186@gmail.com>
2020-05-09 16:01:21 +0900

    シーン変更時キーボードで選択可能

---
+++ TestOtinobo/Assets/SelectButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour
+{
+    void Start()
+    {
+        Selectable select = GetComponent<Selectable>();
+        select.Select();
+    }
+}
