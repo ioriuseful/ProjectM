abb6aaddccf5f7b7e31827af4dd3b7e58889dbf3
佐藤 <satourui7802@gmail.com>
2020-05-27 09:36:44 +0900

    チュートリアルとエンディングシーンの調整

--- TestOtinobo/Assets/Script/UI/TutorialObjGenarateScript.cs
+++ TestOtinobo/Assets/Script/UI/TutorialObjGenarateScript.cs
@@ -6,28 +6,12 @@ public class TutorialObjGenarateScript : MonoBehaviour
 {
     [Header("ゲームシステムって書いてあるやつ")]
     public GameObject GameSystemTutorial;
-    [Header("操作確認って書いてあるやつ")]
-    public GameObject MoveTutorial;
-
-    bool mflag = false;
-    bool sflag = true;
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace) && !sflag)
+        if (Input.GetKeyDown(KeyCode.Backspace))
         {
             Instantiate(GameSystemTutorial, transform.position, Quaternion.identity);
-            sflag = true;
-            mflag = false;
-            Destroy(MoveTutorial);
-        }
-        if (Input.GetKeyDown(KeyCode.Return) && !mflag)
-        {
-            Instantiate(MoveTutorial, transform.position, Quaternion.identity);
-            mflag = true;
-            sflag = false;
-            Destroy(GameSystemTutorial);
+            Destroy(gameObject);
         }
     }
 }
