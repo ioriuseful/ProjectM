89d41708eed8e9f01711c57a98c8689fe7bd7b9f
佐藤 <satourui7802@gmail.com>
2020-06-02 01:34:55 +0900

    タイトルシーンの編集とトゲエネミーの残骸

--- TestOtinobo/Assets/Script/ColorBlockScript.cs
+++ TestOtinobo/Assets/Script/ColorBlockScript.cs
@@ -54,6 +54,7 @@ public class ColorBlockScript : MonoBehaviour
         if(col.gameObject.tag == "Player" && Color == player.Color)
         {
             collider.isTrigger = true;
+            gameObject.tag = "Ground";
             switch(CS)
             {
                 case ColorState.Red:
